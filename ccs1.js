2d5bb9b05950c98e3c63a5de66cb759d3ee6c497
Lucyisnotavailable <147397200+Lucyisnotavailable@users.noreply.github.com>
2023-11-03 16:05:07 +0800

    java sidebar

--- ccs1.js
+++ ccs1.js
@@ -1,10 +1,9 @@
-
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
+document.getElementById('arrow').addEventListener('click', function() {
   const sidebar = document.getElementById('sidebar');
-  if (sidebar.style.left === '10px') 
+  if (sidebar.style.left === '30px') 
   {
     sidebar.style.left = '-800px'; 
   } else {
-    sidebar.style.left = '10px'; 
+    sidebar.style.left = '30px'; 
   }
 });
