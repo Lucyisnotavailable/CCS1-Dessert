669ed68540673c795e60ee1a704381bb361e6895
Lucyisnotavailable <147397200+Lucyisnotavailable@users.noreply.github.com>
2023-11-04 22:12:05 +0800

    11/4

--- ccs1.js
+++ ccs1.js
@@ -16,36 +16,73 @@ document.getElementById('arrow').addEventListener('click', function() {
 const flavorButtons = document.querySelectorAll(".flavor-button");
 
 // 获取要更新的元素
-const dessertTitle = document.querySelector(".dessertstitle2");
-const dessertImage = document.querySelector(".flimg");
-const dessertDescription = document.querySelector(".dessert-description2");
-const dessertReviews=document.querySelector(".user-info")
+const dessertTitle        = document.querySelector(".dessertstitle2");
+const dessertImage        = document.querySelector(".flimg");
+const dessertDescription  = document.querySelector(".dessert-description2");
+const dessertReview1      = document.querySelector(".javatext1");
+const dessertReview2      = document.querySelector(".javatext2");
 
-// 定义不同色块对应的信息
+
 const flavors = {
     flavorbutter: {
-        title: "Silvannas (Butter)",
+        title: "Silvanas (Butter)",
         imageSrc: "silvanasbutterr.jpg",
-        description: "Price: ₱ 310.00<br>-Crunchy and creamy frozen cookies, layered with butter cream sandwiched between two cashew-meringue wafers, coated with cookie crumbs. It is best described as the cookie version of a Sans Rival."
+        description: "Price: ₱ 310.00<br>-Crunchy and creamy frozen cookies, layered with butter cream sandwiched between two cashew-meringue wafers, coated with cookie crumbs. It is best described as the cookie version of a Sans Rival.",
+        reviews1:"★★★★☆<br>It's delicious, but it's too sweet, and you also can't eat too much of it. However, it has a rich flavor and a special texture.",
+        reviews2:"★★★★★<br>Excellent! This dessert is my childhood fav, this is a bit sweet, cold but so good!",
+    },
 
-    },
     flavorchoco: {
-        title: "Silvannas (Chocolate)",
+        title: "Silvanas (Chocolate)",
         imageSrc: "silvannaschocolate.jpg",
-        description: "Price: ₱ 310.00<br>-Although plain versions are the most common, silvanas occasionally come in various flavors such as chocolate, strawberry, mocha, or mango. Popular throughout the country, these cookies have to be refrigerated and are best served well-chilled or frozen."
-    },
-    // more
+        description: "Price: ₱ 330.00<br>-Chocolate Silvanas are a delectable Filipino dessert that consists of layers of chocolate meringue wafers, creamy chocolate buttercream, and a coating of finely crushed cashew nuts. These sweet treats are known for their rich and indulgent chocolate flavor, delicate crispiness from the meringue, and a satisfying nutty crunch from the cashews.",
+        reviews1:"★★★☆☆<br>I'm very picky about chocolate, and this is not good enough for me.",
+        reviews2:"★★★★★<br>I am not fond of chocolate stuff, but this taste good! ",
+      },
+
+    flavorsta: {
+        title: "Silvanas (Strawberry)",
+        imageSrc: "silvannasstrawberryy.jpg",
+        description: "Price: ₱ 325.00<br>-These sweet treats feature layers of strawberry-flavored meringue wafers sandwiched between a luscious strawberry buttercream filling. Strawberry Silvanas provide a refreshing and summery take on this beloved dessert, making them a perfect choice for those who enjoy the sweet and tangy taste of strawberries.",
+        reviews1:"★★★☆☆<br>Chemical strawberry flavor, I think it's average",
+        reviews2:"★★★★★<br>My nephew loves this so much, he keeps requesting for this. This is not so sweet nor so bland.",
+      },
+
+    flavorpanda: {
+        title: "Silvanas (Pandan)",
+        imageSrc: "silvannaspandann.jpg",
+        description: "Price: ₱ 325.00<br>-Buko Pandan meringue rounds with buttercream icing coated with soft cake crumbs. melt-in-your-mouth meringue encases a luscious buttercream icing,creating a dessert that's a perfect balance of textures and flavors.",
+        reviews1:"★★★★☆<br>This one's okay",
+        reviews2:"★★★★★<br>I don’t like Pandan flavors, but when my mum recommended, thinkin’ bout’ buying again next time. Perfect amount of sweetness.",
+      },
+
+    flavortaro: {
+        title: "Silvanas (Ube)",
+        imageSrc: "silvanasube.jpg",
+        description: "Price: ₱ 325.00<br>-A frozen cookie that is crunchy, creamy and delicious with burst of Ube (Purple yam) flavor. Made with cashew meringue wafer with flavored signature buttercream and coated with cake crumbs.",
+        reviews1:"★★★★☆<br>Too sweet, but is good.",
+        reviews2:"★★★★★<br>A bit sweeter than the other flavors but can’t resist the temptation of ube silvanas!",
+      },
+
+
+
+
+
+
 };
 
 
-// 为每个色块添加点击事件处理程序
-flavorButtons.forEach(button => {
+
+flavorButtons.forEach(button =>
+  {
     button.addEventListener("click", function() {
 
         const flavorID = button.id;
         
-        dessertTitle.textContent = flavors[flavorID].title;
-        dessertImage.src = flavors[flavorID].imageSrc;
-        dessertDescription.innerHTML = flavors[flavorID].description;
+        dessertTitle.textContent      = flavors[flavorID].title;
+        dessertImage.src              = flavors[flavorID].imageSrc;
+        dessertDescription.innerHTML  = flavors[flavorID].description;
+        dessertReview1.innerHTML      = flavors[flavorID].reviews1;
+        dessertReview2.innerHTML      = flavors[flavorID].reviews2;
     });
 });
